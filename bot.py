fd0f82968d4ad0375b21fd69d7c7dda3515035bf
Aaro Hämeri <aaro.hameri@aalto.fi>
2018-03-08 18:46:14 +0200

    send images

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 import os
+import picamera as pc
 from dotenv import load_dotenv, find_dotenv
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 
 load_dotenv(find_dotenv())
+
+camera = pc.PiCamera()
 
 token = os.environ.get('BOT_ACCESS_TOKEN')
 print(token)
@@ -15,7 +18,17 @@ dispatcher = updater.dispatcher
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Welcome to Kiltiskahvi bot. ")
 
+def send_pic(bot, update):
+    camera.capture('img.jpg') #todo: put this as file like object instead of writing
+    bot.send_photo(chat_id=update.message.chat_id, photo=open('img.jpg', 'rb')) 
+    bot.send_message(chat_id=update.message.chat_id, text="Welcome to Kiltiskahvi bot. ")
+    os.remove('img.jpg')
+
+
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+send_pic_handler = CommandHandler('pic', send_pic)
+dispatcher.add_handler(send_pic_handler)
+
 
 updater.start_polling()
