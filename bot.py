de86a2940ffe7acfe1aa24ba638d2aa0d89fe84a
Aaro Hämeri <aaro.hameri@aalto.fi>
2018-05-12 12:21:00 +0300

    use in memory file for capturing image

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import io
 import picamera as pc
 from dotenv import load_dotenv, find_dotenv
 from telegram.ext import Updater
@@ -15,15 +16,16 @@ updater = Updater(token=token)
 
 dispatcher = updater.dispatcher
 
+
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Welcome to Kiltiskahvi bot. ")
 
 def send_pic(bot, update):
-    camera.capture('img.jpg') #todo: put this as file like object instead of writing
-    bot.send_photo(chat_id=update.message.chat_id, photo=open('img.jpg', 'rb')) 
-    bot.send_message(chat_id=update.message.chat_id, text="Welcome to Kiltiskahvi bot. ")
-    os.remove('img.jpg')
-
+    stream = io.BytesIO()
+    stream.name = 'image.jpg'
+    camera.capture(stream, 'jpeg') #todo: put this as file like object instead of writing
+    stream.seek(0)
+    bot.send_photo(chat_id=update.message.chat_id, photo=stream) 
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
